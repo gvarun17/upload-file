23bd4efa557975c3c7d713eb854e6ecda889e89b
gvarun17 <gvarunreddy17@gmail.com>
2020-09-06 13:42:37 -0400

    Initial commit

---
+++ NodejsUploadDownloadFileAmazonS3/app/config/s3.env.js
@@ -0,0 +1,8 @@
+const env = {
+	AWS_ACCESS_KEY: 'AKIAVQP2NEJ27YBPS4M6', // change to yours
+	AWS_SECRET_ACCESS_KEY: 'ZPsHAoaiLt0sDOFbqB5MIIfjZrCT5GjKb06eK916', // change to yours
+	REGION : 'us-east-1', // change to yours
+	Bucket: 'test-upload-us-east-1' // change to yours
+};
+ 
+module.exports = env;
